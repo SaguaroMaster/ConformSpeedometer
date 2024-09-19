0001c71ca5830a959d9c4fbd4bed3a6f804d3810
SaguaroMaster <krisi889@gmail.com>
2024-09-19 13:42:54 +0200

    1

--- GPIO_Refresher.py
+++ GPIO_Refresher.py
@@ -9,7 +9,6 @@ ledPin = 40 # Broadcom pin 23 (P1 pin 16)
 # Pin Setup:
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
 GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
-GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
 
 # Initial state for LEDs:
 GPIO.output(ledPin, GPIO.LOW)
@@ -22,5 +21,4 @@ try:
         GPIO.output(ledPin, GPIO.LOW)
         time.sleep(0.5)
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-    pwm.stop() # stop PWM
     GPIO.cleanup() # cleanup all GPIO
