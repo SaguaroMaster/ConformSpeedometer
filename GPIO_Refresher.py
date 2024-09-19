27f59231ab988a2068a7549be9f4760a3638f6c8
SaguaroMaster <krisi889@gmail.com>
2024-09-19 13:51:24 +0200

    1

--- GPIO_Refresher.py
+++ GPIO_Refresher.py
@@ -1,21 +1,14 @@
-# External module imports
 import gpiod
 import time
-
-# Pin Definitons:
-ledPin = 37 # Broadcom pin 23 (P1 pin 16)
-
+LED_PIN = 17
 chip = gpiod.Chip('gpiochip4')
-led_line = chip.get_line(ledPin)
+led_line = chip.get_line(LED_PIN)
 led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-
-
-print("Here we go! Press CTRL+C to exit")
 try:
-    while 1:
-        led_line.set_value(1)
-        time.sleep(0.5)
-        led_line.set_value(0)
-        time.sleep(0.5)
-except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-    led_line.release()
+   while True:
+       led_line.set_value(1)
+       time.sleep(1)
+       led_line.set_value(0)
+       time.sleep(1)
+finally:
+   led_line.release()
