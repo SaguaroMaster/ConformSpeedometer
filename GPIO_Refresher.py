086391a18471bbeacaefa82b1186bfa8a32db4bf
SaguaroMaster <krisi889@gmail.com>
2024-09-23 14:03:18 +0200

    1

--- GPIO_Refresher.py
+++ GPIO_Refresher.py
@@ -37,7 +37,7 @@ if not os.path.isfile(databaseName):
    conn.commit()
    curs.execute("CREATE TABLE settings(timestamp DATETIME, sampling_period NUMERIC, circumference NUMERIC, max_meters NUMERIC, setting1 NUMERIC, setting2 NUMERIC, setting3 NUMERIC, setting4 NUMERIC);")
    conn.commit()
-   curs.execute("INSERT INTO settings values(datetime('now', 'localtime'), 1, 0.25, 5000, 0, 0, 0, 0);")
+   curs.execute("INSERT INTO settings values(datetime('now', 'localtime'), 3, 0.25, 5000, 0, 0, 0, 0);")
    conn.commit()
    conn.close()
 
