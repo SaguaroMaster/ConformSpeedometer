1bf3f473dab9a70f817e54ae5cb89874245ebf9e
SaguaroMaster <krisi889@gmail.com>
2024-09-23 11:08:10 +0200

    1

--- GPIO_Refresher.py
+++ GPIO_Refresher.py
@@ -5,6 +5,8 @@ RELAY_CH1 = 26
 RELAY_CH2 = 20
 RELAY_CH3 = 21
 
+SENSOR_PIN = 18
+
 switchTime = 0.1
 
 chip = gpiod.Chip('gpiochip4')
@@ -12,12 +14,18 @@ RELAY_CH1_Line = chip.get_line(RELAY_CH1)
 RELAY_CH2_Line = chip.get_line(RELAY_CH2)
 RELAY_CH3_Line = chip.get_line(RELAY_CH3)
 
+sensor_line = chip.get_line(SENSOR_PIN)
+
+
 RELAY_CH1_Line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
 RELAY_CH2_Line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
 RELAY_CH3_Line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
 
+sensor_line.request(consumer="Sensor", type=gpiod.LINE_REQ_DIR_IN)
+
 try:
    while True:
+    '''
        RELAY_CH1_Line.set_value(1)
        time.sleep(switchTime)
        RELAY_CH2_Line.set_value(1)
@@ -31,6 +39,12 @@ try:
        time.sleep(switchTime)
        RELAY_CH3_Line.set_value(0)
        time.sleep(switchTime)
+    
+    '''
+
+    sensor_state = button_line.get_value()
+    print(sensor_state)
+
 except KeyboardInterrupt:
     RELAY_CH1_Line.set_value(1)
     time.sleep(switchTime)
