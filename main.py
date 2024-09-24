d6ec375c6b5f21a46c950dd39a562d20346103b0
SaguaroMaster <krisi889@gmail.com>
2024-09-24 14:31:05 +0200

    1

--- main.py
+++ main.py
@@ -42,12 +42,7 @@ def pulseCallback(self):
    global pulseCount, lastPulse, pulseCount2, speed2, maxPulseInterval
    pulseCount = pulseCount + 1
    pulseCount2 = pulseCount2 + 1
-
-   if time.time() > lastPulse + maxPulseInterval:
-         speed2 = 0
-   else:
-      speed2 = 60 / (time.time() - lastPulse) * wheelCircumference
-
+   speed2 = 60 / (time.time() - lastPulse) * wheelCircumference
    lastPulse = time.time()
 
 sensor.when_released = pulseCallback
