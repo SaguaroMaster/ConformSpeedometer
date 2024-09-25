8a8a7585fb71470174abbb0792a5c9b6af9924ac
SaguaroMaster <krisi889@gmail.com>
2024-09-25 08:19:20 +0200

    1

--- main.py
+++ main.py
@@ -180,6 +180,7 @@ root.after(50, root.wm_attributes, '-fullscreen', 'true')
 
 ResetButtonColor = '#ba737e'
 TargetButtonColor = '#82a9d9'
+UnlockButtonColor = '#c9c9c9'
 
 SpeedString = StringVar(value=0)
 LengthString = StringVar(value=0)
@@ -210,7 +211,7 @@ MeterText2 = Label(root, text = 'm', font=('bold', 40)).grid(row=10, column=7, p
 ButtonCounterReset = Button(root, text = 'RESET COUNTER', font=('bold', 25), command = resetLength, height = 2, bg = ResetButtonColor).grid(row=9,column=9, padx=(10,10), columnspan = 9)
 ButtonAlarmReset = Button(root, text = 'RESET ALARM', font=('bold', 25), command = resetAlarm, height = 2, bg = ResetButtonColor).grid(row=11,column=9, padx=(10,10), columnspan = 9)
 
-Unlock = Button(root, text = 'U  N  L  O  C  K', font=('bold', 25), command = unclockSetting, height = 1, width = 27).grid(row=12,column=2, padx=(10,10), columnspan = 5)
+Unlock = Button(root, text = 'U  N  L  O  C  K', font=('bold', 25), command = unclockSetting, height = 1, width = 27, bg = UnlockButtonColor).grid(row=12,column=2, padx=(10,10), columnspan = 5)
 Plus1 = Button(root, text = '+', font=('bold', 40), command = lambda: setLength(1), height = 1, width = 2, bg = TargetButtonColor, state = DISABLED)
 Plus1.grid(row=9,column=6, padx=(10,10))
 Plus10 = Button(root, text = '+', font=('bold', 40), command = lambda: setLength(10), height = 1, width = 2, bg = TargetButtonColor, state = DISABLED)
