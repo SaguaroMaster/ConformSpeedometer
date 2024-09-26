b488aff4faeb51d5263c2f1b7d4e54ae6e9f4f5d
SaguaroMaster <krisi889@gmail.com>
2024-09-26 09:34:02 +0200

    1

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def getSettings():
 def getLastData():
    conn=sqlite3.connect(databaseName)
    curs=conn.cursor()
-   for row in curs.execute("datetime('now', 'localtime')"):
+   for row in curs.execute("DATETIME('now', 'localtime');"):
       time = row[0]
    return time
 
