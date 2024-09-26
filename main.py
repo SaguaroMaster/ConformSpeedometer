f586ba39b8634a3d52bd9b6856194df62ecca4b6
SaguaroMaster <krisi889@gmail.com>
2024-09-26 08:41:22 +0200

    1

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def graphWindowCallback():
    graphWindow = Toplevel(root)
    graphWindow.title(" ")
    #graphWindow.attributes("-fullscreen", True)
-   graphWindow.after(50, graphWindow.wm_attributes, '-fullscreen', 'true')
+   graphWindow.after(50, graphWindow.attributes, '-fullscreen', 'true')
    time.sleep(0.051)
 
    graphWindow.transient(root)
