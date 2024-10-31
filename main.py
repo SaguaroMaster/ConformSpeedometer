f6b02cf91ae1f6a76e6884bfb82cc25ea7fbd002
SaguaroMaster <krisi889@gmail.com>
2024-10-31 13:35:23 +0100

    1

--- main.py
+++ main.py
@@ -257,7 +257,7 @@ def resetLength(): #only resets length measurement
    global alarmState
    length = 0
    pulseCount2 = 0
-   cam.take_photo(imagePath + datetime.now())
+   cam.take_photo(imagePath + str(datetime.now()))
 
 def setAlarm(): #turns alarm sound on for 0.2s then off for 1s. aslo turn on blinker
    relay1.blink(on_time=0.2, off_time=1)
